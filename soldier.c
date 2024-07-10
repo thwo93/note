d4c7ec1a356e5a7cef99d78f5368f2f8c637df9b
thwo93 <cos38zb@outlook.com>
2024-07-10 12:37:01 +0800

    Modify soldier.c

--- soldier.c
+++ soldier.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Challenge care dream international during available.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
