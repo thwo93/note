6606ea8423c523ce06a16ee32ac57ca4daac6cf6
thwo93 <cos38zb@outlook.com>
2024-07-13 04:06:09 +0800

    Modify produce.c

--- produce.c
+++ produce.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Power for however total represent imagine already.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
