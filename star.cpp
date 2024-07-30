28c209918841af8e4dd40fa3e59816d346802e53
thwo93 <cos38zb@outlook.com>
2024-07-30 15:06:05 +0800

    Modify star.cpp

--- star.cpp
+++ star.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Teach inside impact teacher anything how.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
