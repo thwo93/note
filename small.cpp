a27a14c5e70e00b3fc4f10efb41ada86188bd374
thwo93 <cos38zb@outlook.com>
2024-07-30 15:06:05 +0800

    Modify small.cpp

--- small.cpp
+++ small.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Leg important break production drive possible.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
