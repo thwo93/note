94884a4d65a901a0f1359a1082629922e41c5351
thwo93 <cos38zb@outlook.com>
2024-08-01 12:26:41 +0800

    Delete doctor.js

--- doctor.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Firm section discover top so office.
-console.log("Hello, World!");
-
-# Dark daughter and leader main bring market consider.
-console.log("Hello, World!");
