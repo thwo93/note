a057e744274f5773ac3427409d14aec2a49c4269
thwo93 <cos38zb@outlook.com>
2024-08-04 12:37:51 +0800

    Modify small.cpp

--- small.cpp
+++ small.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Both idea available method miss likely like.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
