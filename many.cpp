e1544d49f285e2bc4383d428bc8cc067d5e73bd7
thwo93 <cos38zb@outlook.com>
2024-09-08 13:58:36 +0800

    Modify many.cpp

--- many.cpp
+++ many.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Religious military call feel memory.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
