53a134f088edfa3691e2501d94ad5b7f27258eeb
thwo93 <cos38zb@outlook.com>
2024-09-11 13:24:25 +0800

    Modify begin.cpp

--- begin.cpp
+++ begin.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Treatment tough consider major one style opportunity.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
