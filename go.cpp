83cc98cb7b1c8a258a11fa444bf4dbca8fa82b52
thwo93 <cos38zb@outlook.com>
2024-09-21 10:12:04 +0800

    Modify go.cpp

--- go.cpp
+++ go.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# West sister consider girl former.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
