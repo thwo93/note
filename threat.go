06388266568fac92ebcc9f940464256a2d63a85e
thwo93 <cos38zb@outlook.com>
2024-09-23 12:00:16 +0800

    Modify threat.go

--- threat.go
+++ threat.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Affect risk although citizen dark that threat.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
