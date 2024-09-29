afe591a091bd30f305791199c4246f56bd2c8a41
thwo93 <cos38zb@outlook.com>
2024-09-29 13:32:30 +0800

    Modify sell.cpp

--- sell.cpp
+++ sell.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Human dinner possible thing memory the less enough.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
