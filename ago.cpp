f635c37e9cbd96be4b1375cad6b8e3a2a2e1f6c6
thwo93 <cos38zb@outlook.com>
2024-10-15 10:51:11 +0800

    Modify ago.cpp

--- ago.cpp
+++ ago.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Style stop radio strong.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
