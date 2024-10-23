a8f624a4c968b034a8525d284feb24c1493175b0
thwo93 <cos38zb@outlook.com>
2024-10-23 12:46:33 +0800

    Modify bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Central fear decide name personal almost recognize evening.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
