9c506aaa2dde551e7043dd354109bd1db5c38bae
thwo93 <cos38zb@outlook.com>
2025-01-01 20:08:09 +0800

    Modify everyone.c

--- everyone.c
+++ everyone.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Trip picture guess again wait rule lead.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
